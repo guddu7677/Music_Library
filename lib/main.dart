461fad8754d5d0430b6a8bdc7597903bc674d2cf
guddu7677 <nawalkishore7677@gmail.com>
2026-03-07 02:33:31 +0530

    Create app and folders

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MusicLibraryApp());
+}
+
+class MusicLibraryApp extends StatelessWidget {
+  const MusicLibraryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Music_Library",
+        
+    );
+  }
+}
